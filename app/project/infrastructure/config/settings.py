5994ba29a94d62e1e16a5e1eb79efc8ebde855e4
ChrisAndrei7 <149696921+ChrisAndrei7@users.noreply.github.com>
2024-07-09 08:43:54 -0300

    Update settings.py

--- app/project/infrastructure/config/settings.py
+++ app/project/infrastructure/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e7im26dbj!(m-)^a3#z5&5d($lrk3h+&)ckuk*&gw$@pzp!g8v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.229.103.94', 'ec2-34-229-103-94.compute-1.amazonaws.com', 'localhost']
+ALLOWED_HOSTS = ['54.196.81.55', 'ec2-54-196-81-55.compute-1.amazonaws.com', 'localhost']
 
 
 # application definition
