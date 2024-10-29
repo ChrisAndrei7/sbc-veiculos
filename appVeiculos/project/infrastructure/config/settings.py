b67f243ac13ceed94291c508c382883dacd59ee0
chris <christian_inde@hotmail.com>
2024-10-29 19:29:55 -0300

    Correções

--- appVeiculos/project/infrastructure/config/settings.py
+++ appVeiculos/project/infrastructure/config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'USER': os.environ.get('PG_USER','postgres'),
         'PASSWORD':os.environ.get('PG_PASSWORD','postgres'),
         'NAME': os.environ.get('PG_DB','postgres'),
-        'PORT': os.environ.get('PG_PORT','5434'),
+        'PORT': os.environ.get('PG_PORT','5435'),
         'HOST': os.environ.get('PG_HOST','dbVeiculos'), # uses the container if set, otherwise it runs locally
     }
 }
